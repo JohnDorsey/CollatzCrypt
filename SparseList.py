e7c2f6178455775c33072f258ecb852d6d8acdab
unknown <laptopjohn10@gmail.com>
2017-11-14 15:47:54 -0600

    SparseList class is designed to retain a fixed number of items at similar intervals while being updated with append(item)

---
+++ SparseList.py
@@ -0,0 +1,38 @@
+
+
+class SparseList(list):
+  def __init__(self,inputList,startRun=2,endRun=3):
+    list.__init__(self,inputList)
+    self.spacings = [1 for i in range(len(inputList))]
+    self.startRun = startRun
+    self.endRun = endRun
+    
+  def append(self,item):
+    list.append(self,item)
+    self.spacings.append(1)
+    self.adjust()
+    
+  def bestToRemove(self):
+    recordLowIndex = self.startRun
+    space = self.spacings[recordLowIndex] + self.spacings[recordLowIndex+1]
+    recordLow = space
+    for i in range(self.startRun+1,len(self)-self.endRun):
+      space = self.spacings[i] + self.spacings[i+1]
+      if space < recordLow:
+        recordLow = space
+        recordLowIndex = i
+    return recordLowIndex
+    
+    
+  def __delitem__(self,index):
+    self.spacings[index+1] += self.spacings[index]
+    self.spacings.__delitem__(index)
+    list.__delitem__(self,index)
+    
+  def adjust(self):
+    if len(self) < self.startRun + self.endRun:
+      print("SparseList cannot be made any smaller")
+      return
+    self.__delitem__(self.bestToRemove())
+    
+    
