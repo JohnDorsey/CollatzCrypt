b8b5362390511ab9b593c815671192dea5afe6fb
unknown <laptopjohn10@gmail.com>
2017-11-19 02:38:44 -0600

    added Key class for base conversions

---
+++ Key.py
@@ -0,0 +1,76 @@
+
+
+
+
+
+import math
+
+NUMSTART = ord("0")
+NUMEND = ord("9")
+ALSTART = ord("a")
+ALEND = ord("z")
+AUSTART = ord("A")
+AUEND = ord("Z")
+PRINTSTART = ord(" ")
+PRINTEND = ord("~")
+
+B36 = [chr(item) for gen in [range(NUMSTART,NUMEND+1),range(ALSTART,ALEND+1)] for item in gen]
+B62 = B36 + [chr(item) for item in range(AUSTART,AUEND+1)]
+B64 = B62 + ["-","_"]
+
+def toCharRun(num,a,b):
+  if type(a) == str:
+    a = ord(a)
+  if type(b) == str:
+    b = ord(b)
+  return toCharRange(num,a,a+b)
+
+def toCharRange(num,a,b):
+  if type(a) == str:
+    a = ord(a)
+  if type(b) == str:
+    b = ord(b)
+  base = b - a
+  chars = []
+  for i in range(int(math.log(num,base+1)+1))[::-1]:
+    toAppend = num // (base**i)
+    if toAppend == 0 and len(chars) == 0:
+      print("skipping 0")
+      continue
+    chars.append(chr(toAppend+a))
+    num -= toAppend * (base**i)
+  if num < 0:
+    print("NUM IS " + str(num))
+  return "".join(chars)
+  
+def toCharArr(num,charArr):
+  base = len(charArr)
+  chars = []
+  for i in range(int(math.log(num,base+1)+1))[::-1]:
+    toAppend = num // (base**i)
+    if toAppend == 0 and len(chars) == 0:
+      print("skipping 0")
+      continue
+    chars.append(charArr[toAppend])
+    num -= toAppend * (base**i)
+  if num < 0:
+    print("NUM IS " + str(num))
+  return "".join(chars)
+
+def fromCharRange(string,a,b):
+  if type(a) == str:
+    a = ord(a)
+  if type(b) == str:
+    b = ord(b)
+  base = b - a
+  result = 0
+  for i, char in enumerate(string[::-1]):
+    result += (ord(char)-a)*(base**i)
+ 
+def fromCharArr(string,charArr):
+  base = len(charArr)
+  result = 0
+  for i, val in enumerate(charArr.index(char) for char in string[::-1]):
+    result += val*(base**i)
+  return result
+  
